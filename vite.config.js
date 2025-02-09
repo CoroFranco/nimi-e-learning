1e56265bf74f23c507581f7bdeb75432014b038c
Jhoan Alzate <zhadow.franco@gmail.com>
2025-02-09 15:05:23 -0500

    Agregando vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/normalize.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/normalize.css', 'resources/js/inicioSesion.js', 'resources/js/bootstrap.js', 'resources/js/profile.js'],
             refresh: true,
         }),
     ],
