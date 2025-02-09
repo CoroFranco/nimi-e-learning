52ab71157a8fca8d6a001c8e9e861f3c4a05c761
Jhoan Alzate <zhadow.franco@gmail.com>
2025-02-09 16:08:49 -0500

    Changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/normalize.css', 'resources/js/inicioSesion.js', 'resources/js/bootstrap.js', 'resources/js/profile.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/normalize.css',
+                'resources/js/inicioSesion.js',
+                'resources/js/bootstrap.js',
+                'resources/js/profile.js'
+            ],
             refresh: true,
         }),
     ],
-    base: '/', // Asegura que Vite use rutas relativas
+    server: {
+        https: true,
+    },
+    // Usa una URL dinámica basada en el ambiente
+    base: process.env.APP_URL || 'https://nimi-e-learning-production.up.railway.app',
 });
